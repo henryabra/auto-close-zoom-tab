a1c2092d61c8c662af18f60848f52bcc7ee4eee1
Henry Abravanel <henryabra@gmail.com>
2020-09-11 09:38:58 -0700

    Shorten regex + lower close time

--- background.js
+++ background.js
@@ -1,10 +1,10 @@
 function maybeClose(tabId, changeInfo, tab) {
 	// example url: https://foo.zoom.us/j/634874123#success
 	// example url: https://foo.zoom.us/j/634874123?pwd=fodbarbaz#success
-	if (tab.url.match(/https:\/\/(?:[a-z0-9_]+[.])?zoom.us\/[a-z]\/[0-9]+\/?(?:[?]pwd=[^#&?]+)?[?#]+success/)) {
+	if (tab.url.match(/https:\/\/(?:[a-z0-9_]+\.)?zoom\.us\/[a-z]\/[0-9]+(?:\?pwd=[^#&?]+)?#success/)) {
 		setTimeout(function() {
 			browser.tabs.remove(tab.id);
-		}, 2000);
+		}, 1000);
 	}
 }
 
